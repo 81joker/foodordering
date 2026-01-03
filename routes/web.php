22e28422ad902745204645b22c70057e280a697e
joker81 <nehad.al.timimi@gmail.com>
2026-01-03 20:55:07 +0100

    Add UserController and define CRUD routes for user management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Crud Users
+Route::get('/admin/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/admin/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/admin/users/store', [UserController::class, 'store'])->name('users.store');
+Route::get('/admin/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/admin/users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
