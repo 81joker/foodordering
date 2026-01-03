49c2eac2ab733d24361687a378ea78ff616214c7
Nehad <35563834+81joker@users.noreply.github.com>
2026-01-03 17:14:05 +0100

    Add Booking and Restaurant models with migration files for database tables

---
+++ database/migrations/2026_01_03_152218_create_restaurants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('address');
+            $table->string('phone_number');
+            $table->decimal('delivery_fee', 8, 2)->nullable();
+            $table->decimal('avg_rating', 3, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
