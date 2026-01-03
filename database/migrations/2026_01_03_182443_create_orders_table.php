22cd5905c95d644bf78dc1bac5aa429c8e6c49aa
joker81 <nehad.al.timimi@gmail.com>
2026-01-03 19:46:30 +0100

    Update Laravel workflow and models: re-enable Laravel Pint, update its version, and tidy up Cuisine model imports

--- database/migrations/2026_01_03_182443_create_orders_table.php
+++ database/migrations/2026_01_03_182443_create_orders_table.php
@@ -15,14 +15,13 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('restaurant_id')->constrained('restaurants')->onDelete('cascade');
-            $table->enum('order_status', ['pending', 'confirmed','preparing','ready' ,'on_delivery', 'delivered', 'completed', 'cancelled'])->default('pending');
+            $table->enum('order_status', ['pending', 'confirmed', 'preparing', 'ready', 'on_delivery', 'delivered', 'completed', 'cancelled'])->default('pending');
             $table->decimal('subtotal', 10, 2);
             $table->decimal('delivery_fee', 10, 2);
             $table->decimal('tax', 10, 2);
             $table->decimal('total', 10, 2);
             $table->enum('payment_method', ['credit_card', 'paypal', 'cash_on_delivery']);
             $table->enum('payment_status', ['pending', 'paid', 'failed'])->default('pending');
-            
             $table->timestamps();
         });
     }
