84ccdaf0f659c5c3e5d74ef7e4da317cfcb578cf
Nehad <35563834+81joker@users.noreply.github.com>
2026-01-06 15:52:16 +0100

    Refactor code structure: update UserController to improve readability, clean up ResturantController by removing unused imports, and adjust UserFactory state function formatting.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Models\User;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
         $users = User::paginate(10);
+
         return view('admin.pages.users.index', compact('users'));
     }
 }
