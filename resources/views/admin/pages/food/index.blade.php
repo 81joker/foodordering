f5e52bf3cb76db3c4e730406c190dbaf7f099db0
joker81 <nehad.al.timimi@gmail.com>
2026-03-07 16:16:37 +0100

    fix: Update column span for "No foods found" message in food index view

--- resources/views/admin/pages/food/index.blade.php
+++ resources/views/admin/pages/food/index.blade.php
@@ -80,7 +80,7 @@
                                     </tr>
                                 @empty
                                     <tr>
-                                        <td colspan="7" class="text-center text-muted">No foods found.</td>
+                                        <td colspan="8" class="text-center text-muted">No foods found.</td>
                                     </tr>
                                 @endforelse
                             </tbody>
